7b28951eb2189aabdbb47ce8ced2b723484b4e2f
Chris <chrisloog120@gmail.com>
2024-06-21 19:02:42 +0300

    Add dbcontext and docker compose

---
+++ App.DAL.EF/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using App.Domain;
+using App.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.DAL.EF;
+
+public class AppDbContext : IdentityDbContext<AppUser, AppRole, Guid, IdentityUserClaim<Guid>, AppUserRole,
+    IdentityUserLogin<Guid>, IdentityRoleClaim<Guid>, IdentityUserToken<Guid>>
+{
+    public DbSet<Player> Players { get; set; } = default!;
+    public DbSet<AppUserInTournament> AppUserInTournaments { get; set; } = default!;
+    public DbSet<Competition> Competitions { get; set; } = default!;
+    public DbSet<Country> Countries { get; set; } = default!;
+    public DbSet<Match> Matches { get; set; } = default!;
+    public DbSet<Prediction> Predictions { get; set; } = default!;
+    public DbSet<Team> Teams { get; set; } = default!;
+    public DbSet<TeamInCompetition> TeamInCompetitions { get; set; } = default!;
+    public DbSet<TeamInMatch> TeamInMatches { get; set; } = default!;
+    public DbSet<Tournament> Tournaments { get; set; } = default!;
+    
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entity in ChangeTracker.Entries().Where(e => e.State != EntityState.Deleted))
+        {
+            foreach (var prop in entity
+                         .Properties
+                         .Where(x => x.Metadata.ClrType == typeof(DateTime)))
+            {
+                Console.WriteLine(prop);
+                prop.CurrentValue = ((DateTime) prop.CurrentValue).ToUniversalTime();
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
--- App.Domain/Identity/AppUser.cs
+++ App.Domain/Identity/AppUser.cs
@@ -7,5 +7,8 @@ namespace App.Domain.Identity;
 
 public class AppUser : IdentityUser<Guid>, IDomainEntityId
 {
+    public ICollection<AppUserInTournament>? AppUserInTournaments { get; set; }
+    public ICollection<Tournament>? Tournaments { get; set; }
+    public ICollection<Prediction>? Predictions { get; set; }
     public ICollection<AppRefreshToken>? RefreshTokens { get; set; }
 }
